c63732634e09acd0c21f8e7688681c67b33be535
Arjun Neeliyath <arjun.neeliyath@ust-global.com>
2021-05-13 20:45:09 +0530

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes/routes';
 
 const App = (): ReactElement => (
-  <div className="App">
-    <header className="App-header">
-      <Router>
-        <Routes />
-      </Router>
-    </header>
-  </div>
+    <div className="App">
+        <header className="App-header">
+            <Router>
+                <Routes />
+            </Router>
+        </header>
+    </div>
 );
 export default App;
