3343a92e01090bff70475a3d806ff203813c40bb
Arjun Neeliyath <arjun.neeliyath@ust-global.com>
2021-05-13 13:56:20 +0530

    test commit3

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 import '../App.css';
 import Button from '../components/button/button';
 
@@ -6,19 +6,18 @@ import Button from '../components/button/button';
 const Demo = (): ReactElement => {
   const [inputVal, setInputVal] = useState('');
   var aaa=6;
-
   const onButtonClick = () => {
     setInputVal('');
-  }
+  };
   return (
     <div className="App" data-testid="AppId">
       <header className="App-header">
         <h1>Welcome WSO Demo Page</h1>
         <input placeholder='Type Something' value={inputVal} data-testid='input-id'
-          onChange={e => setInputVal(e.target.value)}/>
+          onChange={e => setInputVal(e.target.value)} />
         <Button data-testid="button-id" key='buttonKey' value='Reset' onClick={onButtonClick} ></Button>
       </header>
     </div >
-  )
-}
+  );
+};
 export default Demo;
