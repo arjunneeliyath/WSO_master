49e177c9a76aa530b83c9ec3f3b391b3d04d92b9
Arjun Neeliyath <arjun.neeliyath@ust-global.com>
2021-05-18 13:14:59 +0530

    Error Boundary Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React, { ReactElement } from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes/routes';
+import ErrorBoundary from './components/errorboundary/ErrorBoundary';
 
 const App = (): ReactElement => (
     <div className="App">
         <header className="App-header">
             <Router>
-                <Routes />
+                <ErrorBoundary>
+                    <Routes />
+                </ErrorBoundary>
             </Router>
         </header>
     </div>
---
+++ src/pages/demo/demo.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement, useState } from 'react';
+import '../../App.css';
+import Button from '../../components/button/button';
+import ErrorBoundary from '../../components/errorboundary/ErrorBoundary';
+
+const Demo = (): ReactElement => {
+    const [inputVal, setInputVal] = useState('');
+    const [myArray, setMyArray] = useState(['']);
+    const onButtonClick = () => {
+        setMyArray([...myArray, inputVal]);
+    };
+    return (
+        <ErrorBoundary>
+            <div className="App" data-testid="AppId">
+                <header className="App-header">
+                    <h1>Welcome WSO Demo Page</h1>
+                    <input
+                        placeholder="Type Something"
+                        value={inputVal}
+                        data-testid="input-id"
+                        onChange={(e) => setInputVal(e.target.value)}
+                    />
+                    <Button data-testid="button-id" key="buttonKey" value="Reset" onClick={onButtonClick}></Button>
+                    {myArray.map((x) => {
+                        return x;
+                    })}
+                </header>
+            </div>
+        </ErrorBoundary>
+    );
+};
+export default Demo;
